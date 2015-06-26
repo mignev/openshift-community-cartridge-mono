96eaa71c7ddcaaaa26720c9574b0a21013daa276
Petko Petkov <p.petkov@cnapsys.com>
2015-06-26 13:52:43 +0300

    add nancyfx as a template

---
+++ template/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace NancyApplication3
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
---
+++ template/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace NancyApplication3
+{
+    using System;
+    using Nancy.Hosting.Self;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string envierenmentUrl = System.Environment.GetEnvironmentVariable("OPENSHIFT_MONO_IP") + System.Environment.GetEnvironmentVariable("OPENSHIFT_MONO_PORT");
+            var uri =
+                new Uri("http://" + envierenmentUrl);
+
+            using (var host = new NancyHost(uri))
+            {
+                host.Start();
+
+                Console.WriteLine("Your application is running on " + uri);
+                Console.WriteLine("Press any [Enter] to close the host.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
